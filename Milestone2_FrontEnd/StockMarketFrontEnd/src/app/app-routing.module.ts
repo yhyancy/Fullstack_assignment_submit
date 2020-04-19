74ed27a8eec271dd880da9d64381e529daaa3243
Hua Yang <dlyhua@cn.ibm.com>
2020-04-19 22:07:26 +0800

    install ng-bootstrap and angular/localize

--- Milestone2_FrontEnd/StockMarketFrontEnd/src/app/app-routing.module.ts
+++ Milestone2_FrontEnd/StockMarketFrontEnd/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),NgbModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
