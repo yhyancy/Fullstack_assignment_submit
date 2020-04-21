a71517e1ce8bad8f66ee008ec9683b994c5287ee
Hua Yang <dlyhua@cn.ibm.com>
2020-04-21 19:12:28 +0800

    add home router

--- Milestone2_FrontEnd/StockMarketFrontEnd/src/app/app-routing.module.ts
+++ Milestone2_FrontEnd/StockMarketFrontEnd/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LoginComponent } from "./components/login/login.component";
 import { SignupComponent } from "./components/signup/signup.component";
+import { HomeComponent } from "./components/home/home.component";
 
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'signup', component: SignupComponent }
+  { path: 'signup', component: SignupComponent },
+  { path: 'home', component: HomeComponent }
 
 ];
 
