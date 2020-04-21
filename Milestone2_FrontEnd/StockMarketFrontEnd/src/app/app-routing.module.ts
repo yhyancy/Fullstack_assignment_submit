9946fd0c65750a0986a3a5f1f9b148b2002e4ac7
Hua Yang <dlyhua@cn.ibm.com>
2020-04-21 15:36:42 +0800

    add login router and login component

--- Milestone2_FrontEnd/StockMarketFrontEnd/src/app/app-routing.module.ts
+++ Milestone2_FrontEnd/StockMarketFrontEnd/src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from "./components/login/login.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes),NgbModule],
+  imports: [RouterModule.forRoot(routes), NgbModule],
+
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
