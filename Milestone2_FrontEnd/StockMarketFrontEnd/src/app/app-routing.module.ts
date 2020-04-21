c8653187acaa5fe296d45a9fa1de77a5b68c8c4e
Hua Yang <dlyhua@cn.ibm.com>
2020-04-21 15:58:21 +0800

    add signup route

--- Milestone2_FrontEnd/StockMarketFrontEnd/src/app/app-routing.module.ts
+++ Milestone2_FrontEnd/StockMarketFrontEnd/src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LoginComponent } from "./components/login/login.component";
+import { SignupComponent } from "./components/signup/signup.component";
 
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent }
+
 ];
 
 @NgModule({
