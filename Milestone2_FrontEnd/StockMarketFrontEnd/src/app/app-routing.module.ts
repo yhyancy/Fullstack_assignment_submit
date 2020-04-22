05244ddcba5b31f9053fdffb5915b98b7e1d0e14
Hua Yang <dlyhua@cn.ibm.com>
2020-04-22 20:38:02 +0800

    add usrhome router and redirect

--- Milestone2_FrontEnd/StockMarketFrontEnd/src/app/app-routing.module.ts
+++ Milestone2_FrontEnd/StockMarketFrontEnd/src/app/app-routing.module.ts
@@ -6,14 +6,25 @@ import { SignupComponent } from "./components/signup/signup.component";
 import { UserhomeComponent } from "./components/userhome/userhome.component";
 import { ChangePasswordComponent } from './components/change-password/change-password.component';
 import { LogoutComponent } from './components/logout/logout.component';
+import { IPOListComponent } from './components/userhome/ipo-list/ipo-list.component';
+import { CompareCompanyComponent } from './components/userhome/compare-company/compare-company.component';
+import { CompareSectorComponent } from './components/userhome/compare-sector/compare-sector.component';
 
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
-  { path: 'userhome', component: UserhomeComponent },
+  {
+    path: 'userhome', component: UserhomeComponent, children: [
+      { path: 'ipolist', component: IPOListComponent },
+      { path: 'compare-company', component: CompareCompanyComponent },
+      { path: 'compare-sector', component: CompareSectorComponent },
+      { path: '**', redirectTo: 'ipolist' }
+    ]
+  },
   { path: 'changepassword', component: ChangePasswordComponent },
   { path: 'logout', component: LogoutComponent }
+
 
 ];
 
