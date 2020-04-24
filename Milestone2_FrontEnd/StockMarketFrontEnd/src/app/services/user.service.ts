0c96aeff2fd20c2d67c267f6070061059136cc89
Hua Yang <dlyhua@cn.ibm.com>
2020-04-25 00:46:39 +0800

    add userService - postLogin to post data

---
+++ Milestone2_FrontEnd/StockMarketFrontEnd/src/app/services/user.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http'; //post数据交互
+import { environment } from '../../environments/environment'
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(public http: HttpClient) { }
+  postLogIn(user) {
+    const httpOtions = { headers: new HttpHeaders({ 'content-Type': 'application/json' }) };
+    return this.http.post(`${environment.baseUrl}/login`, JSON.stringify(user), httpOtions);
+  }
+}
