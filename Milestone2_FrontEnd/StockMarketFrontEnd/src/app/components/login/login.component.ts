b9ac04f13a993dd1f477be8538e9dc33f9c3ca78
Hua Yang <dlyhua@cn.ibm.com>
2020-04-24 15:30:12 +0800

    add login code steps

--- Milestone2_FrontEnd/StockMarketFrontEnd/src/app/components/login/login.component.ts
+++ Milestone2_FrontEnd/StockMarketFrontEnd/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router' //引入router,实现路由的js跳转
 
 @Component({
   selector: 'app-login',
@@ -7,14 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(public router: Router) { } //声明路由
 
   ngOnInit(): void {
   }
 
-  onSignin(value: any) {
+  onSignin(value: any, valid: boolean) {
     console.log(value)
+    console.log(valid)
+    // 验证登录
+    if (!valid) {
+      alert('请检查用户名和密码');
+    }
+    else {
+      //登录成功
+      // TODO: 根据role来判断跳转的URL
+      this.router.navigate(['/userhome']);
+      // TODO: 向后台发送数据
 
+
+    }
   }
 
 }
