a152bfecf2c260490f7995388979d04a334533f4
Hua Yang <dlyhua@cn.ibm.com>
2020-04-25 15:13:31 +0800

    post data to BE

--- Milestone2_FrontEnd/StockMarketFrontEnd/src/app/components/login/login.component.ts
+++ Milestone2_FrontEnd/StockMarketFrontEnd/src/app/components/login/login.component.ts
@@ -16,6 +16,8 @@ const ALERTS: Alert[] = [];
 export class LoginComponent implements OnInit {
   alerts: Alert[];
   constructor(public router: Router, public userService: UserService) {
+    // constructor(public router: Router) {
+
     this.reset();
 
   } //声明路由
@@ -31,17 +33,21 @@ export class LoginComponent implements OnInit {
       //登录验证成功
       // 向后台发送数据
       this.userService.postLogIn(value).subscribe((data) => {
-        if (200 = data.result) {
-          console.log('登录成功')
-          this.alerts.push({ type: 'success', message: 'username or password error!' });
-          // TODO: 根据role来判断跳转的URL
-          this.router.navigate(['/userhome']);
-        } else {
-          console.log('登录失败')
-          this.alerts.push({ type: 'danger', message: 'username or password error!' });
-        }
-
+        // if (200 = data.result) {
+        //   console.log('登录成功')
+        //   this.alerts.push({ type: 'success', message: 'username or password error!' });
+        //   // TODO: 根据role来判断跳转的URL
+        //   this.router.navigate(['/userhome']);
+        // } else {
+        //   console.log('登录失败')
+        //   this.alerts.push({ type: 'danger', message: 'username or password error!' });
+        // }
+        console.log(data)
+        this.router.navigate(['/userhome']);
       })
+
+      //TEST:
+      // this.router.navigate(['/userhome']);
 
     }
   }
--- Milestone2_FrontEnd/StockMarketFrontEnd/src/app/services/user.service.ts
+++ Milestone2_FrontEnd/StockMarketFrontEnd/src/app/services/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http'; //post数据交互
-import { environment } from '../../environments/environment'
+import { environment } from '../../environments/environment' //root url
 @Injectable({
   providedIn: 'root'
 })
