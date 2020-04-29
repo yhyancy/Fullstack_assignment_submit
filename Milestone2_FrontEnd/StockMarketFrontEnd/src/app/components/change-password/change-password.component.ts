33799914bfa8b0f9cbd67f6f09da7fa785adf3e0
Hua Yang <dlyhua@cn.ibm.com>
2020-04-29 11:41:51 +0800

    add changepwd logic

--- Milestone2_FrontEnd/StockMarketFrontEnd/src/app/components/change-password/change-password.component.ts
+++ Milestone2_FrontEnd/StockMarketFrontEnd/src/app/components/change-password/change-password.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-change-password',
@@ -7,13 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChangePasswordComponent implements OnInit {
 
-  constructor() { }
+  constructor(public router: Router) { }
 
   ngOnInit(): void {
   }
   changePwd(value: any, valid: boolean) {
-    alert('修改密码成功')
 
+    alert('修改密码成功')
+    // 前端校验old password 是否和以前一样，并且new password不能和 old password一致
+    // 前端发送新密码 给后端， 格式？
+    // 后端返回200，前端redirect signin 页面，重新登录
+    if (valid) {
+      this.router.navigate(['/login'])
+    }
   }
 
 }
