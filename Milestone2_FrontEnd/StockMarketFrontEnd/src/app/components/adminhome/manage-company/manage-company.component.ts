ac0484ee70a88ce58bcd3a1cfd816a822b661ae6
Hua Yang <dlyhua@cn.ibm.com>
2020-04-30 14:48:00 +0800

    modify manage company edit logic

--- Milestone2_FrontEnd/StockMarketFrontEnd/src/app/components/adminhome/manage-company/manage-company.component.ts
+++ Milestone2_FrontEnd/StockMarketFrontEnd/src/app/components/adminhome/manage-company/manage-company.component.ts
@@ -2,9 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DecimalPipe } from '@angular/common';
 import { QueryList, ViewChildren } from '@angular/core'
 import { Observable } from 'rxjs';
-// import { Country } from '../../Model/country';
 import { COMPANY } from '../../Model/company';
-// import { CountryService } from '../../../services/country.service';
 import { CompanyService } from '../../../services/company.service';
 import { NgbdSortableHeader, SortEvent } from '../../../directives/sortable.directive';
 import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
