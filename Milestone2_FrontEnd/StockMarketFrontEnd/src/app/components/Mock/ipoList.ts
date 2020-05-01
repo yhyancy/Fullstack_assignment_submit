b6ea0c8f3c6237471b900e84cd6dc487c844eaf4
Hua Yang <dlyhua@cn.ibm.com>
2020-05-01 11:30:59 +0800

    add update ipodetail layout

---
+++ Milestone2_FrontEnd/StockMarketFrontEnd/src/app/components/Mock/ipoList.ts
@@ -0,0 +1,60 @@
+import { IPO } from '../Model/ipo'
+export const IPOlist: IPO[] = [
+    {
+        id: 1,
+        companyName: 'KFC',
+        stockExchange: 'ASE',
+        pricePerShare: 356.23,
+        totalNumber: '1356.23',
+        openDateTime: new Date(),
+        remark: 'REMARK1'
+    },
+    {
+        id: 2,
+        companyName: 'KFC',
+        stockExchange: 'ASE',
+        pricePerShare: 356.23,
+        totalNumber: '1356.23',
+        openDateTime: new Date(),
+        remark: 'REMARK1'
+    },
+    {
+        id: 3,
+        companyName: 'KFC',
+        stockExchange: 'ASE',
+        pricePerShare: 356.23,
+        totalNumber: '1356.23',
+        openDateTime: new Date(),
+        remark: 'REMARK1'
+    },
+    {
+        id: 4,
+        companyName: 'KFC',
+        stockExchange: 'ASE',
+        pricePerShare: 356.23,
+        totalNumber: '1356.23',
+        openDateTime: new Date(),
+        remark: 'REMARK1'
+    }
+    ,
+    {
+        id: 5,
+        companyName: 'KFC',
+        stockExchange: 'ASE',
+        pricePerShare: 356.23,
+        totalNumber: '1356.23',
+        openDateTime: new Date(),
+        remark: 'REMARK1'
+    }
+    ,
+    {
+        id: 6,
+        companyName: 'KFC',
+        stockExchange: 'ASE',
+        pricePerShare: 356.23,
+        totalNumber: '1356.23',
+        openDateTime: new Date(),
+        remark: 'REMARK1'
+    }
+
+];
--- Milestone2_FrontEnd/StockMarketFrontEnd/src/app/components/adminhome/update-ipodetails/update-ipodetails.component.ts
+++ Milestone2_FrontEnd/StockMarketFrontEnd/src/app/components/adminhome/update-ipodetails/update-ipodetails.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-
+import { IPO } from '../../Model/ipo'
+import { IPOlist } from '../../Mock/ipoList'
+import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-update-ipodetails',
   templateUrl: './update-ipodetails.component.html',
-  styleUrls: ['./update-ipodetails.component.scss']
+  styleUrls: ['./update-ipodetails.component.scss'],
+  providers: [NgbModalConfig, NgbModal]
 })
 export class UpdateIPODetailsComponent implements OnInit {
+  page = 1;
+  pageSize = 4;
+  collectionSize = IPOlist.length;
+  public ipoList: IPO[] = IPOlist
+  public currentIPO: IPO
 
-  constructor() { }
+
+  get countries(): IPO[] {
+    return IPOlist
+      .map((country, i) => ({ id: i + 1, ...country }))
+      .slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize);
+  }
+
+  constructor(config: NgbModalConfig, private modalService: NgbModal) {
+    config.backdrop = 'static';
+    config.keyboard = false;
+  }
 
   ngOnInit(): void {
+  }
+
+  open(content: any, data: any) {
+    this.currentIPO = data
+    this.modalService.open(content);
   }
 
 }
