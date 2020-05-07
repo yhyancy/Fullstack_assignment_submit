094b9f32f5c338081f0db51c0bd6bb638671a254
Hua Yang <2397856503@qq.com>
2020-05-07 23:23:54 +0800

    modify signup response logic

--- Milestone2_FrontEnd/StockMarketFrontEnd/src/app/components/signup/signup.component.ts
+++ Milestone2_FrontEnd/StockMarketFrontEnd/src/app/components/signup/signup.component.ts
@@ -2,12 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router' //引入Router
 import { UserService } from '../../services/user.service'
 
+
+interface Alert {
+  type: string;
+  message: string;
+}
+
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
+  alerts: Alert[];
 
   constructor(public router: Router, public userService: UserService) { }
 
@@ -17,11 +24,20 @@ export class SignupComponent implements OnInit {
   onSignUp(value: any, valid: boolean) {
     if (valid) {
       //往后台传送数据
-      this.userService.postSignUp(value).subscribe((data) => {
+      this.userService.postSignUp(value).subscribe((data:any) => {
         console.log(data)
+
+           // TODO:判断： 提示信息， 然后跳转
+           if(data.status=="ok"){
+            this.router.navigate(['/login']);
+           }
+           else{
+            this.alerts.push({ type: 'danger', message: 'Please sign up again' });
+            this.router.navigate(['/signup']);
+           }
+
       })
-      // TODO:判断： 提示信息， 然后跳转
-      this.router.navigate(['/login']);
+
 
 
     }
