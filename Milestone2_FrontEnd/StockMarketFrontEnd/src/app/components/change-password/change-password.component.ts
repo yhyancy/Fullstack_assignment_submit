95abac23a9390032365997420f8b8f6ce22e8ba2
Hua Yang <2397856503@qq.com>
2020-05-17 00:23:01 +0800

    modify change pwd logic - be & fe

--- Milestone2_FrontEnd/StockMarketFrontEnd/src/app/components/change-password/change-password.component.ts
+++ Milestone2_FrontEnd/StockMarketFrontEnd/src/app/components/change-password/change-password.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router'
 import { cookieService } from '../../services/cookie.service' // 引入cookie service
+import { UserService } from '../../services/user.service'
 
 @Component({
   selector: 'app-change-password',
@@ -8,20 +9,38 @@ import { cookieService } from '../../services/cookie.service' // 引入cookie se
   styleUrls: ['./change-password.component.scss']
 })
 export class ChangePasswordComponent implements OnInit {
+  public username: any
+  public result: any
 
-  constructor(public router: Router, public cookieService: cookieService) { }
+  constructor(public router: Router, public cookieService: cookieService, public userService: UserService) { }
 
   ngOnInit(): void {
   }
   changePwd(value: any, valid: boolean) {
-
-    alert('修改密码成功')
     // 前端校验old password 是否和以前一样，并且new password不能和 old password一致
     // 前端发送新密码 给后端， 格式？
     // 后端返回200，前端redirect signin 页面，重新登录
     if (valid) {
-      this.cookieService.delUnameCookie()
-      this.router.navigate(['/login'])
+
+      // 发送请求
+      this.userService.postChangePwd(value).subscribe((data) => {
+        this.result = data
+        if (this.result.status == "ok") {
+          //删除cookie
+          this.cookieService.delUnameCookie()
+          // 删除token
+          sessionStorage.removeItem('token')
+          alert('change password successfully.')
+          //重新登录
+          this.router.navigate(['/login'])
+        }
+        else {
+          alert('change password failed.')
+        }
+      })
+
+
+
     }
   }
 
--- Milestone2_FrontEnd/StockMarketFrontEnd/src/app/services/user.service.ts
+++ Milestone2_FrontEnd/StockMarketFrontEnd/src/app/services/user.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http'; //post数据交互
 import { environment } from '../../environments/environment' //root url
+import { cookieService } from '../services/cookie.service' // 引入cookie service
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-
-  constructor(public http: HttpClient) { }
+  public username: any
+  constructor(public http: HttpClient, public cookieService: cookieService) { }
   // 'Access-Control-Allow-Credentials': 'true' 允许跨域
   postLogIn(user: any) {
     // const httpOtions = { headers: new HttpHeaders({ 'content-Type': 'application/json' }) };
@@ -18,6 +19,16 @@ export class UserService {
     const httpOtions = { headers: new HttpHeaders({ 'content-Type': 'application/json', 'Access-Control-Allow-Credentials': 'true' }) };
     // const httpOtions = { headers: new HttpHeaders({ 'content-Type': 'application/json' }) };
     return this.http.post(`${environment.baseUrl}/signup`, JSON.stringify(user), httpOtions)
+  }
+
+  // change pwd
+  postChangePwd(value: any) {
+    // 从cookie里获取uanme
+    this.username = this.cookieService.getUnameCookie()
+    value.username = this.username //添加 username属性
+    console.log(value)
+    const httpOtions = { headers: new HttpHeaders({ 'content-Type': 'application/json', 'Access-Control-Allow-Credentials': 'true' }) };
+    return this.http.post(`${environment.baseUrl}/changepwd`, JSON.stringify(value), httpOtions)
   }
 
   public get currentUserToken(): string {
