0e71c9fac670f1f279c1827da50df5662ce3c861
Hua Yang <2397856503@qq.com>
2020-05-19 23:26:36 +0800

    modify admin/company/list logic

--- Milestone2_FrontEnd/StockMarketFrontEnd/src/app/components/adminhome/managecompany/managecompany.component.ts
+++ Milestone2_FrontEnd/StockMarketFrontEnd/src/app/components/adminhome/managecompany/managecompany.component.ts
@@ -8,17 +8,18 @@ import { ManageCompanyService } from '../../../services/manage-company.service'
   styleUrls: ['./managecompany.component.scss']
 })
 export class ManagecompanyComponent implements OnInit {
-  // 用于接收后台传值
-  companyList: COMPANY[] = []
+
   page = 1;
   pageSize = 4;
   collectionSize: any;
+
+  // companyList用于接收后台的数据
+  public companyList: COMPANY[] = []
   constructor(public manageCompanyService: ManageCompanyService) { }
 
   ngOnInit(): void {
     this.getComapnyList()
   }
-
   getComapnyList() {
     //  后台获取 companylist
     this.manageCompanyService.getCompanyList().subscribe((data: any) => {
@@ -28,11 +29,10 @@ export class ManagecompanyComponent implements OnInit {
       this.collectionSize = this.companyList.length
     })
   }
-
-  get countries(): COMPANY[] {
+  // 分页
+  get CompanyList(): COMPANY[] {
     return this.companyList
-      .map((country, i) => ({ id: i + 1, ...country }))
+      .map((company, i) => ({ id: i + 1, ...company }))
       .slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize);
   }
-
 }
