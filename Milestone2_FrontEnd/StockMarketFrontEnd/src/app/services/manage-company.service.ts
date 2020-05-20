646a7de004726aa55585452b7c9da5245be1b37c
Hua Yang <2397856503@qq.com>
2020-05-20 22:22:33 +0800

    addcompany logic with be

--- Milestone2_FrontEnd/StockMarketFrontEnd/src/app/services/manage-company.service.ts
+++ Milestone2_FrontEnd/StockMarketFrontEnd/src/app/services/manage-company.service.ts
@@ -15,6 +15,14 @@ export class ManageCompanyService {
     return this.http.get(api)
   }
 
+  // 增加公司
+  addCompany(company: any) {
+    console.log(company)
+    const httpOtions = { headers: new HttpHeaders({ 'content-Type': 'application/json' }) }; //固定写法
+    var api = 'http://localhost:8082/admin/company/add'
+    return this.http.post(api, JSON.stringify(company), httpOtions)
+  }
+
   // 停用公司
   disableCompany(company) {
     const httpOtions = { headers: new HttpHeaders({ 'content-Type': 'application/json' }) }; //固定写法
