1f38c75f142d72ea42ba53592e78b88590a1851d
HuaYang <2397856503@qq.com>
2020-06-11 13:05:56 +0800

    修改ipo端口

--- Milestone2_FrontEnd/StockMarketFrontEnd/src/app/services/ipo.service.ts
+++ Milestone2_FrontEnd/StockMarketFrontEnd/src/app/services/ipo.service.ts
@@ -14,7 +14,9 @@ export class IPOService {
   // user/ipo/list
   getUserIPOs() {
     // let api = environment.baseUrl + '/user/ipo/list'
-    let api = "http://localhost:9002/user/ipo/list"
+    // let api = "http://localhost:8083/user/ipo/list"
+    let api = "http://www.fsd.yancystudy.com:8085/yancy/ipo/user/ipo/list"
+
     return this.http.get(api)
   }
 
@@ -22,13 +24,13 @@ export class IPOService {
   // admin/ipo/list
   getAdminIPOs() {
     // let api = environment.baseUrl + '/user/ipo/list'
-    let api = "http://localhost:9002/admin/ipo/list"
+    let api = "http://localhost:8083/admin/ipo/list"
     return this.http.get(api)
   }
 
   // admin/ipo/add
   addIpo(ipo: any) {
-    let api = "http://localhost:9002/admin/ipo/add"
+    let api = "http://localhost:8083/admin/ipo/add"
     const httpOtions = { headers: new HttpHeaders({ 'content-Type': 'application/json', 'Access-Control-Allow-Credentials': 'true' }) };
     return this.http.post(api, JSON.stringify(ipo), httpOtions)
 
