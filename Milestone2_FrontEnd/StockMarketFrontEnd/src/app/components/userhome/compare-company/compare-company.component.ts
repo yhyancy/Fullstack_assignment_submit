981f0b85dc5be2785db6c273bfb8862f4332c672
HuaYang <2397856503@qq.com>
2020-06-14 17:51:24 +0800

    modify echart2

--- Milestone2_FrontEnd/StockMarketFrontEnd/src/app/components/userhome/compare-company/compare-company.component.ts
+++ Milestone2_FrontEnd/StockMarketFrontEnd/src/app/components/userhome/compare-company/compare-company.component.ts
@@ -60,15 +60,15 @@ export class CompareCompanyComponent implements OnInit {
   options = {
     xAxis: {
       type: 'category',
-      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-      // data: []
+      // data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      data: []
     },
     yAxis: {
       type: 'value'
     },
     series: [{
-      data: [820, 932, 901, 934, 1290, 1330, 1320],
-      // data: [],
+      // data: [820, 932, 901, 934, 1290, 1330, 1320],
+      data: [],
       type: 'line'
     }]
   };
